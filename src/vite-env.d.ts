2627c7097cd76a8a90bfc4ac064a088b5aa09fe3
NaMol4anke <r.efremenko13@gmail.com>
2025-09-01 20:39:00 +0300

    Fix: Header

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@
 interface ImportMetaEnv {
     readonly VITE_BACKEND_API_URL: string;
     readonly VITE_FRONTEND_URL: string;
+    readonly VITE_IMG_STORAGE_ENDPOINT_URL: string;
 
     readonly VITE_TG_BOT_TOKEN: string;
     readonly VITE_TG_CHAT_ID: string;
